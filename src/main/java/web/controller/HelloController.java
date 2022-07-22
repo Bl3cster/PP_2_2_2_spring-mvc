205fc2fe7a05f2ff6153a2a0412a4281ae6ce809
Bl3cter <blecster@gmail.com>
2022-07-23 00:49:24 +0300

    ex2_2_2

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,24 +1,14 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import web.Service.CarServiceImpl;
-import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
-
-	private final CarServiceImpl carService;
-	@Autowired
-	public HelloController(CarServiceImpl carService){
-		this.carService=carService;
-	}
 
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
@@ -29,10 +19,5 @@ public class HelloController {
 		model.addAttribute("messages", messages);
 		return "index";
 	}
-	@GetMapping(value = "/cars")
-	public String showCars(@RequestParam(defaultValue = "5") Integer count, ModelMap model) {
-		List<Car> car = carService.giveSomeCars(count);
-		model.addAttribute("cars", car);
-		return "cars";
-	}
+
 }
